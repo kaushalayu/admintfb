ce13c46f977cfbd894f582d390dd4158e7d9809e
Ayush Gupta <kaushalayush95@gmail.com>
2026-06-16 17:52:13 +0530

    done

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,6 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ? `${import.meta.env.VITE
 
 const api = axios.create({
   baseURL: API_BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
 })
 
 api.interceptors.request.use((config) => {
